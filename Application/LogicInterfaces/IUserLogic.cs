8bd5480e0cedd57a56bf1adb73abc4e8dc2cb432
Casillas0905 <ikersanmartinbarreiro@gmail.com>
2023-03-19 14:08:38 +0100

    Add user

---
+++ Application/LogicInterfaces/IUserLogic.cs
@@ -0,0 +1,10 @@
+﻿using Shared.DTOs;
+using Shared.Models;
+
+namespace Application.LogicInterfaces;
+
+public interface IUserLogic
+{
+    Task<User> CreateAsync(UserCreationDto userToCreate);
+    
+}
